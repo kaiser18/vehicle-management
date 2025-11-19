9b075ec32ec7a289335efcf0321bf428fd4a0a51
kaiser18 <mihailoivic@protonmail.com>
2025-11-19 17:53:08 +0100

    implemented validation on frontend

--- frontend/src/components/VehicleForm.tsx
+++ frontend/src/components/VehicleForm.tsx
@@ -15,6 +15,8 @@ const VehicleForm = ({onSave}: VehicleFormProps) => {
         mileage: ''
     });
 
+    const [error, setError] = useState('');
+
     const handleOnChange = (event : React.ChangeEvent<HTMLInputElement>) => {
         setFormData({
             ...formData,
@@ -24,16 +26,36 @@ const VehicleForm = ({onSave}: VehicleFormProps) => {
 
     const handleOnSubmit = async (event : React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        try {
+        if (formData.model.trim() === '' || formData.firstRegistrationYear.trim() === '' || formData.cubicCapacity === 0 || formData.fuel.trim() === '' || formData.mileage.trim() === '') {
+            setError('Please fill in all fields.');
+            return;
+        } else if (formData.model.trim().length > 40) {
+            setError('Model name is too long (max 40 characters).');
+            return;
+        } else if (!formData.firstRegistrationYear.match(/^\d{4}$/g)) {
+            setError('First registration year must be exactly 4 digits.');
+            return;
+        } else if (formData.cubicCapacity > 9999) {
+            setError('Cubic capacity is too high (max 9999).');
+            return;
+        } else if (!formData.mileage.trim().match(/^\d{1,7}$/g)) {
+            setError('Mileage is too long (max 7 digits).');
+            return;
+        } else {
+            try {
             await addVehicle(formData);
+            setError('');
             onSave();
-        } catch (error) {
-            console.error("Error adding vehicle:", error);
+            } catch (error) {
+                console.error("Error adding vehicle:", error);
+            }
         }
+        
     };
 
     return (
         <Form onSubmit={handleOnSubmit} className="vehicle-form">
+            {error && <p className="errorMsg">{error}</p>}
             <FormGroup controlId='model'>
                 <Form.Label>Model</Form.Label>
                 <Form.Control
@@ -60,6 +82,7 @@ const VehicleForm = ({onSave}: VehicleFormProps) => {
                     className="cubicCapacity"
                     name="cubicCapacity" 
                     type="number"
+                    min="0"
                     value={formData.cubicCapacity}
                     onChange={handleOnChange}
                 />
